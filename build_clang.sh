565926c8b3ac44d5ce55eb85fbe6bf315298a815
etr1hc <Thong.LeTrung@vn.bosch.com>
2019-10-25 11:32:42 +0200

    update script'

--- build_clang.sh
+++ build_clang.sh
@@ -11,4 +11,4 @@ mkdir -p build
 cd build
 cmake -DLLVM_ENABLE_PROJECTS=clang -G "Unix Makefiles" ../llvm
 make
-
+make clang
--- example/02.fuzzer/build.sh
+++ example/02.fuzzer/build.sh
@@ -2,5 +2,5 @@ gcc -fsanitize=address -ggdb -o gcc.out hello_world.c
 
 ./gcc.out
 
-../../submodule/llvm-project/build/bin/clang++  -fsanitize=address  -rt   -g -O1  -o clang.out  hello_world.c
+../../submodule/llvm-project/build/bin/clang++  -fsanitize=address   -g -O1  -o clang.out  hello_world.c
 ./clang.out
